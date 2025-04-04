6fa726dcf75de8afb149f202b845b09bef7c1928
Dominik Heidler <dheidler@suse.de>
2025-04-04 14:52:31 +0200

    Support lua5.4 in Makefile
    Build succeeds against 5.4

--- check.c
+++ check.c
@@ -2,8 +2,8 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
-#if (LUA_VERSION_NUM < 501) || (LUA_VERSION_NUM > 503)
-# error "Lua 5.1, 5.2, 5.3 required"
+#if (LUA_VERSION_NUM < 501) || (LUA_VERSION_NUM > 504)
+# error "Lua 5.1, 5.2, 5.3, 5.4 required"
 #endif
 
 int main (void) {
